8011d2b4da672d7474feec96fb21f14237bb5761
Zoha Fard <72476695+zohaf@users.noreply.github.com>
2021-02-26 15:01:28 +1100

    node is setted up

---
+++ index.js
@@ -0,0 +1,28 @@
+const fs = require("fs");
+const axios = require("axios");
+const { html } = require("cheerio");
+const $ = require("cheerio");
+
+const sourceURL = "https://www.madcoversite.com/dmd-alphabetical.html";
+
+axios.get(sourceURL).then((response) => {
+  const html = response.data;
+  const $rows = $("table table tr:nth-child(n+2)", html);
+  const entries = [];
+
+  $rows.each(function () {
+    const $cells = $("td", this);
+    const entry = {
+      sound: $cells.eq(0).text(),
+      description: $cells.eq(1).text(),
+      source: $cells.eq(2).text(),
+      title: $cells.eq(3).text(),
+    };
+
+    entries.push(entry);
+  });
+
+  fs.writeFile("./martin.json", JSON.stringify(entries), () => {
+    console.log(`martin.json saved: ${entries.length} entries`);
+  });
+});
